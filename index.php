d4efca639ca9eb820415db7e9da383203e7b9c70
gabrielarodriguezova <gabrielaova13@gmail.com>
2026-02-19 19:43:40 -0600

    se agragó el index page con links de CV's

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemón</title>
+</head>
+<body>
+    <h1>Bienvenido al proyecto ABC</h1>
+    <br>
+    <img src="./ZOODEX.png" alt="Imagen de equipo" width="150px" height="150px">
+    <br>
+    <h2>Integrantes del equipo:</h2>
+    <ul>
+        <li>
+            <a href="./Anenqui">Descarga el cv de Anenqui Yolatl Pintado Venegas</a>
+        </li>
+        <li>
+            <a href="./Erick">Descarga el cv de Erick Solórzano López</a>
+        </li>
+        <li>
+            <a href="./Rodrigo">Descarga el cv de Rodrigo Jordan Hernandez Cruz</a>
+        </li>
+        <li>
+            <a href="./Mario">Descarga el cv de Juan Mario Morales Castellanos</a>
+        </li>
+        <li>
+            <a href="./Gaby.docx">Descarga el cv de Gabriela Rodríguez Ovando</a>
+        </li>
+    <ul>
+</body>
+</html>
